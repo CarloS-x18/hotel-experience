8066d57f5feaaa9f8b77c245aedf38089bba0c55
Carlos Alberto <carlosescobedomoreno@gmail.com>
2021-08-16 22:11:09 -0500

    Header finalizado

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 const btnMenu = document.querySelector('#btn-menu');
 const menuView = document.querySelector('#links');
 
+// menu event toggle to navbar
 btnMenu.addEventListener('click', () => {
     menuView.classList.toggle('view-menu');
 });
