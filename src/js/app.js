07021865ec7e10b38976f300745c71e26b6c28eb
Carlos Alberto <carlosescobedomoreno@gmail.com>
2021-08-18 08:29:11 -0500

    Nav scroll function terminada, cards de ofertas con detalles finalizadas

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,22 @@
 // variables 
 const btnMenu = document.querySelector('#btn-menu');
 const menuView = document.querySelector('#links');
+const nav = document.querySelector('#navegacion');
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('scroll', navScroll);
+})
 
 // menu event toggle to navbar
 btnMenu.addEventListener('click', () => {
     menuView.classList.toggle('view-menu');
     btnMenu.classList.toggle('button-rotate');
 });
+
+function navScroll() {
+    if(window.scrollY !== 0) {
+        nav.classList.add('bg-white');
+    } else {
+        nav.classList.remove('bg-white');
+    }
+}
