890260579a4a7142dd243194087015d343962911
Carlos Alberto <carlosescobedomoreno@gmail.com>
2021-08-19 19:20:31 -0500

    Proyecto finalizado, v1.0

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@
 const btnMenu = document.querySelector('#btn-menu');
 const menuView = document.querySelector('#links');
 const nav = document.querySelector('#navegacion');
+const links = document.querySelectorAll('.link__section');
+const linksContainer = document.querySelector('.nav__links');
 
 document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('scroll', navScroll);
@@ -20,3 +22,33 @@ function navScroll() {
         nav.classList.remove('bg-white');
     }
 }
+
+// scroll in page section view
+links.forEach(link => {
+    link.addEventListener('click', e => {
+
+        e.preventDefault();
+
+        // variables
+        const sectionLink = e.currentTarget.getAttribute("href").slice(1);
+        const sectionPage = document.querySelector(`#${sectionLink}`);
+        const navHeight = nav.getBoundingClientRect().height;
+
+        const linksHeight = linksContainer.getBoundingClientRect().height;
+        const navFixed = linksContainer.classList.contains('view-menu');
+        let position = sectionPage.offsetTop - navHeight;
+
+        if(navFixed) {
+            position += linksHeight;
+        }
+
+        window.scrollTo({
+            left: 0,
+            top: position
+        });
+
+        menuView.classList.remove('view-menu');
+        btnMenu.classList.remove('button-rotate');
+    });
+});
+
